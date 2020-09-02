b0d3e58beb7bea2e6518294e996df9ad90dfa93e
DaltonSparks <dalton.sparks@mavs.uta.edu>
2020-09-02 16:08:30 -0500

    P02 fc First Commit

---
+++ P02/full_credit/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+    srand(time(NULL));
+
+    int dice, faces, rolls, sum, i, j;
+
+    std::cout << "Number of dice? ";
+    std::cin >> dice;
+
+    std::cout << "Number of Faces per die? ";
+    std::cin >> faces;
+    faces++;
+
+    std::cout << "Number of rolls? ";
+    std::cin >> rolls;
+
+    for(i = 0; i < rolls; i++)
+    {
+		sum = 0;
+        std::cout << "Roll " << i << ":";        
+
+        for(j = 0; j < dice; j++)
+        {
+			int num = rand()%faces;
+            std::cout << " " << num;
+			sum = sum+num;
+        }
+        std::cout << " Sum: " << sum;
+        std::cout << "\n";
+    }
+}
